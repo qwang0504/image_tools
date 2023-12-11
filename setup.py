f1e0d15800cacb1857c06d799b1b1a4052209095
ElTinMar <martin.privat@hotmail.fr>
2023-12-11 17:52:53 +0100

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "numpy", 
         "scipy",
-        "opencv-python",
+        "https://github.com/ElTinmar/build_opencv/raw/main/opencv_contrib_python_rolling-4.8.0.20231210-cp38-cp38-linux_x86_64.whl",
         "scikit-image",
         "PyQt5",
         "geometry @ git+https://github.com/ElTinmar/geometry.git@main",
