3569152cd78299b3ba7f40e0a186323712f2a23b
ElTinMar <martin.privat@hotmail.fr>
2023-12-14 17:40:59 +0100

    remove gpu fun

--- image_tools/morphology.py
+++ image_tools/morphology.py
@@ -4,13 +4,6 @@ from numpy.typing import NDArray
 from typing import Tuple, Optional
 from skimage.measure import regionprops
 
-try:
-    import cupy as cp
-    from cupy.typing import NDArray as CuNDArray
-    from cupyx.scipy import ndimage as cu_ndi
-    from cucim.skimage.measure import regionprops as cu_regionprops
-except:
-    print('No GPU available, cupy not imported')
 
 # those are essentially stripped down versions of 
 # skimage.morphology.remove_small_objects
@@ -23,14 +16,6 @@ def label(
     strel = ndi.generate_binary_structure(ar.ndim, connectivity)
     return ndi.label(ar, strel)[0]
 
-def label_GPU(        
-        ar: CuNDArray, 
-        connectivity: int = 1
-    ) -> CuNDArray:
-
-    strel = cu_ndi.generate_binary_structure(ar.ndim, connectivity)
-    return cu_ndi.label(ar, strel)[0]
-
 def properties(
         ar: NDArray, 
         connectivity: int = 1
@@ -39,14 +24,6 @@ def properties(
     label_img = label(ar, connectivity)
     return regionprops(label_img)
 
-def properties_GPU(
-        ar: CuNDArray, 
-        connectivity: int = 1
-    ) -> list:
-
-    label_img = label_GPU(ar, connectivity)
-    return cu_regionprops(label_img)
-
 def components_size(
         ar: NDArray, 
         connectivity: int = 1
@@ -54,15 +31,6 @@ def components_size(
     
     ccs = label(ar, connectivity)
     component_sz = np.bincount(ccs.ravel()) 
-    return (component_sz, ccs)
-
-def components_size_GPU(
-        ar: CuNDArray, 
-        connectivity: int = 1
-        ) -> Tuple[CuNDArray, CuNDArray]:
-    
-    ccs = label_GPU(ar, connectivity)
-    component_sz = cp.bincount(ccs.ravel()) 
     return (component_sz, ccs)
 
 def bwareaopen(
@@ -79,20 +47,6 @@ def bwareaopen(
 
     return out
 
-def bwareaopen_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64, 
-        connectivity: int = 1
-        ) -> CuNDArray:
-    
-    out = ar.copy()
-    component_sz, ccs = components_size_GPU(ar, connectivity)
-    too_small = component_sz < min_size
-    too_small_mask = too_small[ccs]
-    out[too_small_mask] = 0
-
-    return out
-
 def bwareaclose(
         ar: NDArray, 
         max_size: int = 256, 
@@ -101,20 +55,6 @@ def bwareaclose(
     
     out = ar.copy()
     component_sz, ccs = components_size(ar, connectivity)
-    too_big = component_sz > max_size
-    too_big_mask = too_big[ccs]
-    out[too_big_mask] = 0
-
-    return out
-
-def bwareaclose_GPU(
-        ar: CuNDArray, 
-        max_size: int = 256, 
-        connectivity: int = 1
-        ) -> CuNDArray:
-    
-    out = ar.copy()
-    component_sz, ccs = components_size_GPU(ar, connectivity)
     too_big = component_sz > max_size
     too_big_mask = too_big[ccs]
     out[too_big_mask] = 0
@@ -139,24 +79,6 @@ def bwareafilter(
 
     return out
 
-def bwareafilter_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64, 
-        max_size: int = 256, 
-        connectivity: int = 1
-        ) -> CuNDArray:
-    
-    out = ar.copy()
-    component_sz, ccs = components_size_GPU(ar, connectivity)
-    too_small = component_sz < min_size 
-    too_small_mask = too_small[ccs]
-    too_big = component_sz > max_size
-    too_big_mask = too_big[ccs]
-    out[too_small_mask] = 0
-    out[too_big_mask] = 0
-
-    return out
-
 def bwareaopen_centroids(
         ar: NDArray, 
         min_size: int = 64,
@@ -164,16 +86,6 @@ def bwareaopen_centroids(
     ) -> NDArray:
     
     props = properties(ar, connectivity)
-    centroids = [blob.centroid[::-1] for blob in props if blob.area > min_size]
-    return np.asarray(centroids, dtype=np.float32)
-
-def bwareaopen_centroids_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64,
-        connectivity: int = 1,
-    ) -> CuNDArray:
-
-    props = properties_GPU(ar, connectivity)
     centroids = [blob.centroid[::-1] for blob in props if blob.area > min_size]
     return np.asarray(centroids, dtype=np.float32)
 
@@ -203,32 +115,6 @@ def bwareafilter_centroids(
         centroids.append([x, y])
     return np.asarray(centroids, dtype=np.float32)
 
-def bwareafilter_centroids_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64, 
-        max_size: int = 256, 
-        min_length: Optional[int] = None,
-        max_length: Optional[int] = None,
-        min_width: Optional[int] = None,
-        max_width: Optional[int] = None,
-        connectivity: int = 1
-    ) -> CuNDArray:
-
-    props = properties_GPU(ar, connectivity)
-    centroids = []
-    for blob in props:
-        if not (min_size < blob.area < max_size):
-            continue
-        if (min_width is not None) and (max_width is not None):
-            if not (min_width < 2*blob.axis_minor_length < max_width):
-                continue
-        if (min_length is not None) and (max_length is not None):
-            if not (min_length < 2*blob.axis_major_length < max_length):
-                continue
-        y, x = blob.centroid
-        centroids.append([x, y])
-    return cp.asarray(centroids, dtype=np.float32)
-
 def bwareaopen_props(
         ar: NDArray, 
         min_size: int = 64, 
@@ -236,15 +122,6 @@ def bwareaopen_props(
     ) -> list:
 
     props = properties(ar, connectivity)
-    return [blob for blob in props if blob.area > min_size]
-
-def bwareaopen_props_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64, 
-        connectivity: int = 1
-    ) -> list:
-
-    props = properties_GPU(ar, connectivity)
     return [blob for blob in props if blob.area > min_size]
 
 def bwareafilter_props(
@@ -259,31 +136,6 @@ def bwareafilter_props(
     ) -> list:
 
     props = properties(ar, connectivity)
-    filtered_props = []
-    for blob in props:
-        if not (min_size < blob.area < max_size):
-            continue
-        if (min_width is not None) and (max_width is not None):
-            if not (min_width < 2*blob.axis_minor_length < max_width):
-                continue
-        if (min_length is not None) and (max_length is not None):
-            if not (min_length < 2*blob.axis_major_length < max_length):
-                continue
-        filtered_props.append(blob)
-    return filtered_props
-
-def bwareafilter_props_GPU(
-        ar: CuNDArray, 
-        min_size: int = 64, 
-        max_size: int = 256, 
-        min_length: Optional[int] = None,
-        max_length: Optional[int] = None,
-        min_width: Optional[int] = None,
-        max_width: Optional[int] = None,
-        connectivity: int = 1
-    ) -> list:
-
-    props = properties_GPU(ar, connectivity)
     filtered_props = []
     for blob in props:
         if not (min_size < blob.area < max_size):
