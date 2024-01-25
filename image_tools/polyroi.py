198782e7cdbb926cff784674d4ca961f144c91f7
ElTinmar <martin.privat@hotmail.fr>
2024-01-25 18:57:35 +0100

    reverse

--- image_tools/polyroi.py
+++ image_tools/polyroi.py
@@ -30,12 +30,10 @@ def polyroi(img: NDArray) -> NDArray:
         cv2.imshow('image', local_image) 
 
     cv2.namedWindow('image')
-    #cv2.setMouseCallback('image', click_event)
+    cv2.setMouseCallback('image', click_event)
     cv2.imshow('image', local_image) 
     cv2.waitKey(0) 
-    #cv2.destroyWindow('image') 
-
-    coords = [(0,0),(0,10),(10,10)]
+    cv2.destroyWindow('image') 
     
     return np.array(coords, np.int32)
 
