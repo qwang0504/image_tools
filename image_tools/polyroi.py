9e05dfef183c62a50de7642a02c47b9c9343da35
ElTinmar <martin.privat@hotmail.fr>
2024-01-25 18:18:01 +0100

    destroy all window

--- image_tools/polyroi.py
+++ image_tools/polyroi.py
@@ -27,13 +27,13 @@ def polyroi(img: NDArray) -> NDArray:
         # so I make a copy
         original = img_rgb.copy() 
         local_image = cv2.polylines(original, [pts], True, (0, 0, 255), 1)
-        #cv2.imshow('image', local_image) 
+        cv2.imshow('image', local_image) 
 
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', click_event)
     cv2.imshow('image', local_image) 
     cv2.waitKey(0) 
-    cv2.destroyWindow('image') 
+    cv2.destroyAllWindows()
     
     return np.array(coords, np.int32)
 
