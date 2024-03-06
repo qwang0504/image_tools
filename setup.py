90d9970e4a0ec8a934bde805d50014b6e519f2f4
ElTinmar <martin.privat@hotmail.fr>
2024-03-07 00:14:11 +0100

    added polyROI gui

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='image_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.3.4',
+    version='0.3.5',
     packages=['image_tools','image_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='image processing functions',
