f62701dcf04fc27fd908f2ad93bc1a6f80e65653
ElTinmar <martin.privat@hotmail.fr>
2024-03-29 14:13:14 +0100

    rgb2gray and im2gray

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='image_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.4.9',
+    version='0.4.10',
     packages=['image_tools','image_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='image processing functions',
