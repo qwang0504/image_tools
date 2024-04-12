05ab87d2cd47444994f33682fff55ff653e0418b
ElTinmar <martin.privat@hotmail.fr>
2024-04-12 15:29:18 +0200

    add conversions

--- image_tools/convert_gpu.py
+++ image_tools/convert_gpu.py
@@ -85,3 +85,73 @@ def im2single_GPU(input_image: CuNDArray) -> CuNDArray:
         raise ValueError('wrong image type, cannot convert to single')
 
     return single_image
+
+def im2gray_GPU(input_image: CuNDArray) -> CuNDArray:
+    """
+    Transform color input into grayscale by taking only the first channel
+
+    Inputs:
+        input_image: M x N x C | M x N x C x K numpy array 
+
+    Outputs:
+        M x N | M x N x K numpy array 
+    """
+
+    shp = input_image.shape
+
+    if len(shp) == 2:
+        # already grayscale, nothing to do
+        return input_image
+    
+    if len(shp) >= 3:
+        return input_image[...,0]
+    
+    else:
+        raise ValueError('wrong image type, cannot convert to grayscale')
+    
+
+def im2uint8_GPU(input_image: CuNDArray) -> CuNDArray:
+    '''Convert image to uint8.'''
+    
+    if cp.issubdtype(input_image.dtype, cp.integer):
+        if input_image.dtype == cp.uint8:
+            return input_image
+        
+        ui_info = cp.iinfo(input_image.dtype)
+        uint8_image = (input_image *  255.0/ui_info.max).astype(cp.uint8)
+
+    elif cp.issubdtype(input_image.dtype, cp.floating):
+        uint8_image = (input_image *  255.0).astype(cp.uint8)
+
+    elif input_image.dtype == cp.bool_:
+        uint8_image = 255 * input_image.astype(cp.uint8) 
+
+    else:
+        raise ValueError('wrong image type, cannot convert to uint8')
+
+    return uint8_image
+
+def im2double_GPU(input_image: CuNDArray) -> CuNDArray:
+    """
+    Transform input image into a double precision floating point image
+    """
+
+    if cp.issubdtype(input_image.dtype, cp.integer):
+        # if integer type, transform to float and scale between 0 and 1
+        ui_info = cp.iinfo(input_image.dtype)
+        double_image = input_image.astype(cp.float64) / ui_info.max
+
+    elif cp.issubdtype(input_image.dtype, cp.floating):
+        # if already a floating type, convert to double precision
+        if input_image.dtype == cp.float64:
+            return input_image
+        
+        double_image = input_image.astype(cp.float64)
+
+    elif input_image.dtype == cp.bool_:
+        double_image = input_image.astype(cp.float64) 
+
+    else:
+        raise ValueError('wrong image type, cannot convert to double')
+    
+    return double_image
